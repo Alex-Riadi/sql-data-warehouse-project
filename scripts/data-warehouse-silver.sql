aa66c1a541dae04137239674d03252a56e1e88ce
Alex-Riadi <alexriadi99@gmail.com>
2025-06-03 14:17:26 +0800

    revision

--- scripts/data-warehouse-silver.sql
+++ scripts/data-warehouse-silver.sql
@@ -1,7 +1,7 @@
 --INITIAL CHECK & DATA EXPLORATION
-SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_cust_info];
+SELECT TOP (0) * FROM [DataWarehouse].[bronze].[crm_cust_info];
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_prd_info];
-SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[crm_sales_details];
+SELECT TOP (0) * FROM [DataWarehouse].[bronze].[crm_sales_details];
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_cust_az12];
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_loc_a101];
 SELECT TOP (1000) * FROM [DataWarehouse].[bronze].[erp_px_cat_g1v2]
